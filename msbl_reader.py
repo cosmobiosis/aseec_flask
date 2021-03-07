e3895fa9051d7c194240d4e1a6b29b71444e26d5
cosmobiosis <32723573+cosmobiosis@users.noreply.github.com>
2021-03-07 13:28:05 -0800

    add prefix for pages

--- msbl_reader.py
+++ msbl_reader.py
@@ -90,9 +90,10 @@ class MaximBootloader(object):
                 buf_copy = deepcopy(tmp_page.data)
                 # self.msbl.pages_data.append(buf_copy)
                 page_base64 = []
+                prefix_bytes = bytes.fromhex("01030004")
                 for trunk_ind in range(32):
-                    page_base64.append(self.get_base64_string(bytearray(buf_copy[trunk_ind * 256: (trunk_ind + 1) * 256])))
-                page_base64.append(self.get_base64_string(bytearray(buf_copy[8192:])))
+                    page_base64.append(self.get_base64_string(prefix_bytes + bytearray(buf_copy[trunk_ind * 256: (trunk_ind + 1) * 256])))
+                page_base64.append(self.get_base64_string(prefix_bytes + bytearray(buf_copy[8192:])))
                 self.msbl.pages_base64.append(page_base64)
                 total_size = total_size + sizeof(tmp_page)
                 # print(self.msbl.pages_data[i])
